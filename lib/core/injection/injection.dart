146ccc08e70e330b1745f3aa38feaa7afeb4cf49
Salvador Frisco <salvadorfrisco70@gmail.com>
2025-02-06 16:47:10 -0300

    refactor: discard unnecessary file

--- lib/core/injection/injection.dart
+++ lib/core/injection/injection.dart
@@ -2,11 +2,7 @@ import 'package:http/http.dart' as http;
 
 import '../../data/datasources/motel_datasource.dart';
 import '../../data/repositories/motel_repository_impl.dart';
-import '../../domain/usecases/get_motel.dart';
-import '../../presentation/controllers/motel_controller.dart';
 
 final httpClient = http.Client();
 final motelDatasource = MotelDatasourceImpl(httpClient);
 final motelRepository = MotelRepositoryImpl(datasource: motelDatasource);
-final getMotel = GetMotel(motelRepository);
-final motelController = MotelController(getMotel);
